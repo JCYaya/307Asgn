b88361bf749fb6b74e0a206b9b18666b4131d2f6
Jocya <jocyaya@gmail.com>
2024-01-15 00:57:08 -0800

    start to add form

--- packages/react-frontend/src/MyApp.js
+++ packages/react-frontend/src/MyApp.js
@@ -1,27 +1,11 @@
 // src/MyApp.js
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 // src/MyApp.js
 function MyApp() {
-  const [characters, setCharacters] = useState([
-    {
-      name: "Charlie",
-      job: "Janitor"
-    },
-    {
-      name: "Mac",
-      job: "Bouncer"
-    },
-    {
-      name: "Dee",
-      job: "Aspring actress"
-    },
-    {
-     name: "Dennis",
-      job: "Bartender"
-    }
-   ]);
+  const [characters, setCharacters] = useState([]);
 
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
@@ -32,12 +16,13 @@ function MyApp() {
 
   return (
   <div className="container">
-    <Table characterData={characters} 
-     removeCharacter={removeOneCharacter}
+    <Table
+      characterData={characters}
+      removeCharacter={removeOneCharacter}
     />
+    <Form />
   </div>
   );
-
 }
 
 export default MyApp;
