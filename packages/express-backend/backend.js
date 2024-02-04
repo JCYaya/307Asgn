90ddf581c1bdc94158dfd2031ff30d7cd9ca54a4
Jocya <jocyaya@gmail.com>
2024-02-03 16:52:59 -0800

    fetch by user id

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -3,16 +3,17 @@ import express from "express";
 
 const app = express();
 const port = 8000;
+
 const users = {
 	users_list: [
-		{
-		    id: "xyz789",
-			 name: "Charlie",
-			 job: "Janitor"
-		},
 	   {
-		   id: "abc123",
-		   name: "Mac",
+         id: "xyz789",
+	     	name: "Charlie",
+	      job: "Janitor"
+	   },
+	   {
+       	id: "abc123",
+	      name: "Mac",
 	      job: "Bouncer"
 	   },
 	   {
@@ -32,15 +33,31 @@ const users = {
 	   }
    ]
 };
+
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
    res.send("Hello World!");
 });
 
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
 app.get("/users", (req, res) => {
    res.send(users);
 });
+
+
 
 app.listen(port, () => {
    console.log(
